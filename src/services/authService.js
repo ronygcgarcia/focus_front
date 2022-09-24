874680797eed718ee755030ecff6c3e28e9fffcd
Rony Garcia <ronygcgarcia@gmail.com>
2022-09-23 23:57:21 -0600

    feat: adds index and creation user

--- src/services/authService.js
+++ src/services/authService.js
@@ -19,6 +19,21 @@ const getUser = async () => {
   const { data: user } = await axios.get("/user");
 
   return user;
-}
+};
 
-export { login, getRoutes, getUser };
+const getRoles = async () => {
+  const { data: roles } = await axios.get("/roles");
+  return roles;
+};
+
+const storeUser = async (user) => {
+  const { data: userResponse } = await axios.post("/users", user);
+  return userResponse;
+};
+
+const getUsers = async () => {
+  const { data: users } = await axios.get("/users");
+  return users;
+};
+
+export { login, getRoutes, getUser, getRoles, storeUser, getUsers };
